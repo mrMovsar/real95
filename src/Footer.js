cf851dc7986f1b20cf09fa4e8e583776e7990f9a
Movsar <movsar1991.mb@gmail.com>
2020-11-17 10:59:17 +0300

    props

--- src/Footer.js
+++ src/Footer.js
@@ -4,10 +4,12 @@ import FooterMenu from "./FooterMenu";
 import CopyRight from "./CopyRight";
 function Footer(){
     return(
+        <div className="footerWrap">
         <div className="footer">
             <FooterLogo/>
             <FooterMenu/>
             <CopyRight/>
+        </div>
         </div>
     )
 }
